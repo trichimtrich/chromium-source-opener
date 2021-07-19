2199ce5f8c19bc7be385829a7b6faccb246d1279
Song Fangzhen <songfangzhen@bytedance.com>
2021-07-19 16:10:30 +0800

    Add essential files
    Add files which are necessary for clones building.

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-// const open = require('open');
 import * as open from 'open';
 
 // this method is called when your extension is activated
